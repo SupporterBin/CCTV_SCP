5bff1ed37c1986bff7e77e94db7e40d3d8332de0
LHS <gustjs1234596@gmail.com>
2025-10-23 16:33:16 +0900

    이상현상3제작완료(서있는거)

--- Assets/Scripts/Anomaly/AnomalyEvent/Anomaly_Restraint.cs
+++ Assets/Scripts/Anomaly/AnomalyEvent/Anomaly_Restraint.cs
@@ -3,19 +3,52 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Anomalies/Event/Anomaly_Restraint")]
 public class Anomaly_Restraint : BasicEventAnomaly
 {
+    public GameObject chairPrefab;
+    public GameObject monsterPrefab;
+
+    private GameObject[] spawnObj;
+
+    [Header("서있는 몬스터 모습 생성 위치 및 회전값")]
+    public Vector3 monster_Transform;
+    public Quaternion monster_Quaternion;
 
     public override EventType Execute()
     {
-        throw new System.NotImplementedException();
+        spawnObj = new GameObject[2];
+
+        GameObject OrginMonster = GameManager.Instance.anomalySystem.monsters[2];
+
+        // 기존 몬스터의 보이는 부분을 비활성화
+        OrginMonster.transform.GetChild(0).gameObject.SetActive(false);
+
+        // --- 몬스터 생성 (로컬 좌표 적용) ---
+        spawnObj[0] = Instantiate(monsterPrefab, OrginMonster.transform);
+        spawnObj[0].transform.localPosition = monster_Transform;
+        spawnObj[0].transform.localRotation = monster_Quaternion;
+
+        spawnObj[1] = Instantiate(chairPrefab, OrginMonster.transform);
+
+        return eventType;
     }
 
     public override void Clear()
     {
-        throw new System.NotImplementedException();
+        Destroy(spawnObj[0]);
+        Destroy(spawnObj[1]);
+
+        GameManager.Instance.anomalySystem.monsters[2].transform.GetChild(0).gameObject.SetActive(true);
+
+        Debug.Log("끝, 성공");
     }
 
     public override void Fail()
     {
-        throw new System.NotImplementedException();
+        Destroy(spawnObj[0]);
+        Destroy(spawnObj[1]);
+
+        GameManager.Instance.anomalySystem.monsters[2].transform.GetChild(0).gameObject.SetActive(true);
+
+
+        Debug.Log("끝, 실패");
     }
 }
